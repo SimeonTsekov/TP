4c5f84a02f2714e33826b5b107b925bb1cbed15a
Simeon Tsekov <simeontsekov@pop-os.localdomain>
2019-12-06 08:58:34 +0200

    changed files to the 06.12 version

--- Flask_codes_class/post.py
+++ Flask_codes_class/post.py
@@ -1,14 +1,11 @@
 from database import DB
-from comment import Comment
-
 
 class Post:
-    def __init__(self, id, name, author, content, category):
+    def __init__(self, id, name, author, content):
         self.id = id
         self.name = name
         self.author = author
         self.content = content
-        self.category = category
 
     @staticmethod
     def all():
@@ -19,47 +16,11 @@ class Post:
     @staticmethod
     def find(id):
         with DB() as db:
-            row = db.execute(
-                'SELECT * FROM posts WHERE id = ?',
-                (id,)
-            ).fetchone()
+            row = db.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
             return Post(*row)
-
-    @staticmethod
-    def find_by_category(category):
-        with DB() as db:
-            rows = db.execute(
-                'SELECT * FROM posts WHERE category_id = ?',
-                (category.id,)
-            ).fetchall()
-            return [Post(*row) for row in rows]
 
     def create(self):
         with DB() as db:
-            values = (self.name, self.author, self.content, self.category.id)
-            db.execute('''
-                INSERT INTO posts (name, author, content, category_id)
-                VALUES (?, ?, ?, ?)''', values)
+            values = (self.name, self.author, self.content)
+            row = db.execute('INSERT INTO posts (name, author, content) VALUES (?, ?, ?)', values)
             return self
-
-    def save(self):
-        with DB() as db:
-            values = (
-                self.name,
-                self.author,
-                self.content,
-                self.category.id,
-                self.id
-            )
-            db.execute(
-                '''UPDATE posts
-                SET name = ?, author = ?, content = ?, category_id = ?
-                WHERE id = ?''', values)
-            return self
-
-    def delete(self):
-        with DB() as db:
-            db.execute('DELETE FROM posts WHERE id = ?', (self.id,))
-
-    def comments(self):
-        return Comment.find_by_post(self)
